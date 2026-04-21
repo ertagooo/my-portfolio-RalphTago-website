e9f12f126bc1bf3e8ecc461ae87b9d9f83372a22
ert.tago <ert.tago@gmail.com>
2026-04-21 21:16:55 +0800

    restore homepage render

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <h1>Portfolio Loaded</h1>
-
-    <!-- your pages go here -->
-  </div>
+  <HomePage />
 </template>
+
+<script setup>
+import HomePage from './pages/HomePage.vue'
+</script>
